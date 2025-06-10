d7fe9f26c399db1b48073d646e4547e6190be993
Benedikt Krings <bkrings@officewin.databay.de>
2025-06-10 15:51:41 +0200

    Adding doc folder for documentation files like README and install requirements

--- testing energiedaten v3.py
+++ testing energiedaten v3.py
@@ -5,7 +5,7 @@
 # Version: 2025061007                                                 #
 #          YYYYMMDD Change Number                                     #
 #######################################################################
-# If modules are not installed, please install them via pip, note for this in file "install requirements.txt"
+# If modules are not installed, please install them via pip, note for this in file "install requirements.txt" in the doc/ folder.
 # Theirs a list of all required modules and how to install them.
 # Version 2.7 LTS (Long Term Support) of the energiedaten-app
 
